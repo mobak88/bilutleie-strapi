54391bd5297fdcd4f2f9cf2b47fa51e5759a017b
Renatebh <renate.hem@gmail.com>
2022-08-17 11:35:09 +0200

    added app keys and apiToken.salt

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '30bb14696a476158a612b7fa83e9d285'),
+    secret: env("ADMIN_JWT_SECRET", "30bb14696a476158a612b7fa83e9d285"),
+  },
+  apiToken: {
+    salt: "30bb14696a476158a612b7fa83e9d285",
   },
 });
