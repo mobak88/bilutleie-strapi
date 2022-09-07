59be3b6a8310d921ff5f2e931e5887d4e3d67423
mobak88 <mostafa0401@ymail.com>
2022-09-07 15:42:14 +0200

    Adding car image link

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,5 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
@@ -14,6 +13,6 @@ module.exports = ({ env }) => ({
                 rejectUnauthorized: false
             },
         },
+        debug: false,
     },
-    debug: false
 });
